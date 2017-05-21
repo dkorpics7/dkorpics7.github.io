45b3dc88d87de0722daf46afcd373daac826d25f
dkorpics7 <dkorpics@roadrunner.com>
2017-05-21 19:30:40 -0400

    Formatted main page and added popups to About page and added quotes

---
+++ js/site.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    //toggle headshot picture upon mouseover
+    $(".headshot").mouseover(function () {
+        $(this).attr('src', $(this).data("hover"));
+    });
+    $(".headshot").mouseout(function () {
+        $(this).attr('src', $(this).data("src"));
+    });
+
+    //pop-ups open and close for "many hats" page//
+    //----- OPEN
+    $('[data-popup-open]').on('click', function (e) {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+        e.preventDefault();
+        $('.people img').hide();
+        $('.people p').hide();
+    });
+
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function (e) {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+        e.preventDefault();
+        $('.people img').show();
+        $('.people p').show();
+    });
+
+});
